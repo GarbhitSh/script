6c140bad060c34247a2b7f65edf5cff267c39682
garbhitsh <garbhitsharmaemail@gmail.com>
2025-01-01 19:14:29 +0530

    commit

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 
 
 import plotly.express as px
-import cv2
-import torch
-import matplotlib.pyplot as plt
-import torch
-import random
-import time
-import os
-import sqlite3
-import pandas as pd
-import matplotlib.pyplot as plt
